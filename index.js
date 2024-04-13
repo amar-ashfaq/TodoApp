9e6dd656c980e46d96889f0412fb60532b1e95a5
Amar AA <amar.ashfaq95@gmail.com>
2024-04-13 23:32:10 +0100

    Implemented the 'createTodo' function

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const input = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
 document.addEventListener("DOMContentLoaded", () => {
+    getTodos();
+})
+
+function createTodo() {
+
+    addBtn.addEventListener('click', () => {
+        
+        console.log('Adding new todo...');
+
+        // append to the todoList
+        let todo = document.createElement('li');
+        todo.textContent = input.value.trim();
+        todoList.appendChild(todo);
+        input.value = '';
+        
+        console.log(todo);
+        console.log(todoList);
+    })
+}
+
+createTodo();
+
+function getTodos() {
+
+    input.value = '';
+
     fetch("https://jsonplaceholder.typicode.com/todos")
         .then((todos) => {
             if (todos.ok) {
@@ -21,5 +47,5 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .catch((error) => {
             console.error("Error:" + error);
-        });   
-})
+        });
+}
